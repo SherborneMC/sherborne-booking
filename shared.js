3b3d28910a6fa2f73b3e45f5af5fc7031fba40e3
SherborneMC <michael.alpha.public@gmail.com>
2026-06-21 06:04:12 +0100

    Delete shared.js

--- shared.js
+++
@@ -1,8 +0,0 @@
-import { buildGrid } from './shared.js'
-
-export async function onRequestGet({env}){
-  return new Response(
-    JSON.stringify(await buildGrid(env,env.OWNER_EMAIL)),
-    { headers:{'content-type':'application/json'} }
-  )
-}
