5607944dc08773b234e13b20fedf0a13063ae30a
SherborneMC <michael.alpha.public@gmail.com>
2026-06-21 06:19:22 +0100

    Update availability.js

--- functions/api/availability.js
+++ functions/api/availability.js
@@ -1,8 +1,5 @@
 import { buildGrid } from './shared.js'
 
 export async function onRequestGet({env}){
-  return new Response(
-    JSON.stringify(await buildGrid(env,env.OWNER_EMAIL)),
-    { headers:{'content-type':'application/json'} }
-  )
-} 
+ return new Response(JSON.stringify(await buildGrid(env,env.OWNER_EMAIL)))
+}
